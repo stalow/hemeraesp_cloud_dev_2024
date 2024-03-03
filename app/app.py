9dee8624e4b9b3f0955051f97b24bc8df9324788
Hicham Benali <hichambenali1999@gmail.com>
2024-03-03 02:18:05 +0100

    fix bug python

--- app/app.py
+++ app/app.py
@@ -1,6 +1,11 @@
 import paho.mqtt.client as mqtt
 import time
 
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    client.subscribe("/hem/testhem")
+
 # Callback appelée lorsqu'un message est reçu depuis le topic
 def on_message(client, userdata, msg):
     print(f"Message reçu sur le topic {msg.topic}: {msg.payload.decode()}")
@@ -9,10 +14,9 @@ def on_message(client, userdata, msg):
 
 # Configuration du client MQTT
 mqtt_client = mqtt.Client()
+mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
 
-mqtt_client = mqtt.Client()
-mqtt_client.on_message = on_message
 
 # Connexion au broker MQTT (Mosquitto) avec informations d'identification
 mqtt_client.username_pw_set("HEMERAESP", "8J7Nq4e9Ci8wTe")
