1b5d25c081e3b137709dc9b941d34dc632c6eab1
Hicham Benali <hichambenali1999@gmail.com>
2024-03-03 01:43:05 +0100

    fix bug python

--- app/app.py
+++ app/app.py
@@ -25,12 +25,13 @@ mqtt_client.connect("mqtt", 1883, 60)
 mqtt_client.subscribe("/hem/testhem")
 
 # Démarrage du client MQTT en arrière-plan
-mqtt_client.loop_start()
+#mqtt_client.loop_start()
+client.loop_forever()
 
 
-@app.route('/dontgohere')
+"""@app.route('/dontgohere')
 def hello_world():
     return 'Hello, Docker Compose with Python and Mosquitto!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)"""
