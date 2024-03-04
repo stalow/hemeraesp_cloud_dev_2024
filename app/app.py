7a3ddad73d076f5783aa58bfed002d864080cb9d
Hicham Benali <hichambenali1999@gmail.com>
2024-03-04 14:15:52 +0100

    pub file

--- app/app.py
+++ app/app.py
@@ -4,11 +4,35 @@ import time
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-    client.subscribe("/hem/testhem")
+    client.subscribe("/hem/endprocess")
 
 # Callback appelée lorsqu'un message est reçu depuis le topic
 def on_message(client, userdata, msg):
-    print(f"Message reçu sur le topic {msg.topic}: {msg.payload.decode()}")
+    if msg.topic == "/hem/endprocess":
+        print(f"Message reçu sur le topic {msg.topic}: {msg.payload.decode()}")
+
+        # Nom du fichier CSV
+        csv_filename = "/data/donnees.csv"
+
+        # Extraction des données du message MQTT
+        payload = msg.payload.decode()
+
+        # Ouvrir le fichier CSV en mode append (ajout)
+        with open(csv_filename, mode='a', newline='') as csv_file:
+            csv_file.write(f"{msg.payload.decode()}\n")
+
+    if msg.topic == "/hem/getdata":
+        # Nom du fichier CSV pour la publication avec le topic "takedata"
+        csv_filename = "/data/donnees.csv"
+
+        # Extraction des données du message MQTT
+        #payload = msg.payload.decode()
+
+        # Écrire le message dans le fichier CSV avec le topic "takedata"
+        with open(csv_filename, mode='r', newline='') as csv_filename:
+            csv_content = csv_file.read()
+            
+        client.publish("/hem/rcvdata", csv_content)
 
 print("MQTT sub")
 
@@ -23,7 +47,7 @@ mqtt_client.username_pw_set("HEMERAESP", "8J7Nq4e9Ci8wTe")
 
 # Connexion au broker MQTT (Mosquitto)
 mqtt_client.connect("mqtt", 1883, 60)
-mqtt_client.subscribe("/hem/testhem")
+mqtt_client.subscribe("/hem/endprocess")
 
 # Démarrage du client MQTT en arrière-plan
 mqtt_client.loop_start()
